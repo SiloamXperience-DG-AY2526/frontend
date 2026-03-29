8e3286fa8f3be4d3d6a276f13383e074813f239d
Tasmiyah <tasmiyahtaj26@gmail.com>
2026-03-29 12:43:53 +0800

    resolved bugs and copilot issues

--- src/lib/utils/finance-manager-email/insertAtCursor.tsx
+++
@@ -1,8 +0,0 @@
-export function insertAtCursor(el: HTMLTextAreaElement, value: string) {
-  const start = el.selectionStart ?? el.value.length;
-  const end = el.selectionEnd ?? el.value.length;
-  el.value = el.value.slice(0, start) + value + el.value.slice(end);
-  const cursor = start + value.length;
-  el.setSelectionRange(cursor, cursor);
-  el.dispatchEvent(new Event('input', { bubbles: true }));
-}
